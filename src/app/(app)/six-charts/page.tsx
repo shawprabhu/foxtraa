94387730c58dc6c9904dd699914289d196f1c26c
Arshad-dev90 <hussainarshad19059@gmail.com>
2025-01-24 15:57:43 +0530

    added sixchat pg

---
+++ src/app/(app)/six-charts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SixCharts = () => {
+  return (
+    <div>
+        <h1 className='text-3xl texte-center '>Six Charts Coming soon</h1>
+    </div>
+  )
+}
+
+export default SixCharts
