a0d55dbae30ee1f4fc620cb47c19f6eae1703326
Arshad-dev90 <hussainarshad19059@gmail.com>
2025-01-31 16:22:51 +0530

    added head for gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,17 +30,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-PFDLGNX4');`,
-        }}
-      />
+          }}
+        />
+      </head>
       <AuthProvider>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased dark `}
